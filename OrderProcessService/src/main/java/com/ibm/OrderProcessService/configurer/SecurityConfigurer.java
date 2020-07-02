aed77fb183edb9226270cea7bb0898ece0f5b864
w2cluster249 <w2cluster249@w2cluster281.iiht.tech>
2020-07-02 15:57:59 +0530

    Spring Security Added in Order Process Service

--- OrderProcessService/src/main/java/com/ibm/OrderProcessService/configurer/SecurityConfigurer.java
+++ OrderProcessService/src/main/java/com/ibm/OrderProcessService/configurer/SecurityConfigurer.java
@@ -2,13 +2,18 @@ package com.ibm.OrderProcessService.configurer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.ibm.OrderProcessService.service.*;
+
 
 @EnableWebSecurity
 public class SecurityConfigurer  extends WebSecurityConfigurerAdapter{
@@ -16,8 +21,9 @@ public class SecurityConfigurer  extends WebSecurityConfigurerAdapter{
 
 	@Autowired
 	private MyUserDetailsService myuserDetailsService;
-
 	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,24 +31,30 @@ public class SecurityConfigurer  extends WebSecurityConfigurerAdapter{
 	super.configure(auth);
 	}
 	
-	@Override
-	  protected void configure(HttpSecurity http) throws Exception {
-	    http
-	        .authorizeRequests()
-	        .antMatchers("/").permitAll()
-	                                     
-	        .antMatchers("/admin/**").hasRole("ADMIN")
-	          .and()
-	        .csrf().disable() 
-	        .headers().frameOptions().disable()
-	          .and()
-	        .formLogin();
-	  }
-	
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+		
+				.authorizeRequests().antMatchers("/authenticate","/h2-console/**").permitAll().
+				
+						anyRequest().authenticated().and().
+						exceptionHandling().and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		 httpSecurity.headers().frameOptions().disable();
+
 	}
 	
 }
--- OrderProcessService/src/main/java/com/ibm/OrderProcessService/controller/OrderProcessController.java
+++ OrderProcessService/src/main/java/com/ibm/OrderProcessService/controller/OrderProcessController.java
@@ -2,19 +2,36 @@ package com.ibm.OrderProcessService.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm.OrderProcessService.configurer.AuthenticationRequest;
+import com.ibm.OrderProcessService.configurer.AuthenticationResponse;
+import com.ibm.OrderProcessService.configurer.JwtUtil;
 import com.ibm.OrderProcessService.models.OderDTO;
 import com.ibm.OrderProcessService.service.MyUserDetailsService;
+
+
 
 @RestController
 public class OrderProcessController {
 
 @Autowired
-private MyUserDetailsService service;
+private MyUserDetailsService userDetailsService;
+
+@Autowired
+private AuthenticationManager authenticationManager;
+
+@Autowired
+private JwtUtil jwtTokenUtil;
 	
 	
 	@GetMapping("/hello")
@@ -23,10 +40,34 @@ private MyUserDetailsService service;
 	}
 	
 	
-	  @PostMapping("/save") public ResponseEntity<OderDTO> saveOrder(@RequestBody
-	  OderDTO orderDTO){ OderDTO dto=service.saveOrder(orderDTO); return
+	 @RequestMapping(value = "/save", method = RequestMethod.POST)
+	  public ResponseEntity<OderDTO> saveOrder(@RequestBody
+	  OderDTO orderDTO){ OderDTO dto=userDetailsService.saveOrder(orderDTO);
+	  return
 	  ResponseEntity.ok().body(dto);
 	  
 	  }
+	  
+	  @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+		public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+			try {
+				authenticationManager.authenticate(
+						new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+				);
+			}
+			catch (BadCredentialsException e) {
+				throw new Exception("Incorrect username or password", e);
+			}
+
+
+			final UserDetails userDetails = userDetailsService
+					.loadUserByUsername(authenticationRequest.getUsername());
+
+			final String jwt = jwtTokenUtil.generateToken(userDetails);
+
+			return ResponseEntity.ok(new AuthenticationResponse(jwt));
+		}
+
 	 
 }
